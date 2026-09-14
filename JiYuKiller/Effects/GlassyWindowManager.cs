3f0bf10cb0c1a44f7793fd3c58382e43ce395d76
QD <qd@local.dev>
2026-09-14 23:36:26 +0800

    修复所有自定义设置默认值：GlassyManager._blurIntensity 0.9->0.8, OuterGlow/InnerGlow Opacity 0.2/0.3->0.05, Text显示同步修改

--- JiYuKiller/Effects/GlassyWindowManager.cs
+++ JiYuKiller/Effects/GlassyWindowManager.cs
@@ -28,7 +28,7 @@ namespace JiYuKiller.Effects
         private bool _isCapturing;
         private bool _isDeactivatedCapture;
         private GlassyEffect _glassyEffect;
-        private double _blurIntensity = 0.9;
+        private double _blurIntensity = 0.8;
 
         public GlassyWindowManager(Window window, Border backdropBorder, Border glassyBorder)
         {
