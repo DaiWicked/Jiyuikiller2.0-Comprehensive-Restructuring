306e4471875aa85647f89db80f1b7569ff974400
QD <qd@local.dev>
2026-09-20 23:14:18 +0800

    修复任务栏图标:代码中BitmapImage加载ICO

--- GameRoom/MainWindow.xaml.cs
+++ GameRoom/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ namespace GameRoom
         public MainWindow()
         {
             InitializeComponent();
+            try { this.Icon = new System.Windows.Media.Imaging.BitmapImage(new Uri("pack://application:,,,/GameRoom;component/Assets/game.ico")); } catch { }
             Loaded += MainWindow_Loaded;
             Closed += MainWindow_Closed;
         }
